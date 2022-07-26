c13b9982fbbf0dd0432d63e3bab21e3762d85a5c
karl-no <karl.publicidade@gmail.com>
2022-07-25 21:29:08 -0300

    Requisito 2 - A Vingança

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Form extends React.Component {
-  //   constructor() {
-  //     super();
-  //     this.state =
-  //   }
   render() {
     const {
       cardName,
